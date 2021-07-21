40f2e63d63ddcd5d1ef9e4c0dea7130b330d2e7e
turner <csusbdt@gmail.com>
2021-07-20 21:35:28 -0700

    X

--- docs/box/index.js
+++ docs/box/index.js
@@ -17,10 +17,12 @@ const grid   = g.loop(g.frames(i_grid  ));
 
 const b_loop  = g.loop(g.frames(i_box));
 const d1_loop = g.loop(g.frames(i_d1));
+const u1_loop = g.loop(g.frames(i_u1));
+const l2_loop = g.loop(g.frames(i_l2));
 
-const b_down = g.once(g.frames(i_blank)).starts(() => {
-	b_loop.dy += 50;
-})
+// const b_down = g.once(g.frames(i_blank)).starts(() => {
+// 	b_loop.dy += 50;
+// });
 
 const t = [ new Array(4), new Array(4) ];
 for (let c = 0; c < t.length; ++c) {
@@ -32,26 +34,30 @@ for (let c = 0; c < t.length; ++c) {
 }
 
 const b  = { c: 0, r: 0};
-const d1 = { c: 1, r: 1};
+const u1 = { c: 0, r: 3};
 
-const step_down = () => {	
-};
+// const step_down = () => {	
+// };
 
 for (let c = 0; c < t.length; ++c) {
 	for (let r = 0; r < t[0].length; ++r) {
 		t[c][r].starts(() => {
-			const with_d1 = d1.c === b.c && d1.r === b.r;
+			const with_u1  = u1.c === b.c && u1.r === b.r;
 			if (b.c < c) {
+				// go right
+				if (with_u1) u1.c += 1;
 				b.c += 1;
-				if (with_d1) d1.c = b.c;
 			} else if (b.c > c) {
+				// go left
+				if (with_u1) u1.c -= 1;
 				b.c -= 1;
-				if (with_d1) d1.c = b.c;
 			} else if (b.r < r) {
+				// go down
+				if (with_u1) u1.r += 1;
 				b.r += 1;
 			} else {
+				// go up
 				b.r -= 1;
-				if (with_d1) d1.r = b.r;
 			}
 			view();
 		});
@@ -61,34 +67,44 @@ for (let c = 0; c < t.length; ++c) {
 const view = () => {
 	b_loop.set_dx(b.c   * 200);
 	b_loop.set_dy(b.r   * 200);
-	d1_loop.set_dx(d1.c * 200);
-	d1_loop.set_dy(d1.r * 200);
-	if (b.c === 0 && b.r === t[0].length - 1 && (d1.c !== 0 || d1.r !== t[0].length)) {
+	u1_loop.set_dx(u1.c * 200);
+	u1_loop.set_dy(u1.r * 200);
+
+	const with_u1     = u1.c === b.c && u1.r === b.r;
+	const over_u1     = u1.c === b.c && u1.r === b.r + 1;
+	const under_u1    = u1.c === b.c && u1.r === b.r - 1;
+	const left_of_u1  = u1.c === b.c + 1 && u1.r === b.r;
+	const right_of_u1 = u1.c === b.c - 1 && u1.r === b.r;
+
+	if (!with_u1 && b.c === 0 && b.r === 3) {
 		g.delay(.5).starts(() => {
 			b_loop.set_dx(b.c - 190);
 		}).start();
 		return;
 	}
+
 	if (b.c > 0) {
-		// left
-		if (d1.c !== b.c - 1 || d1.r !== b.r) {
+		// consider left
+		if (!right_of_u1) {
 			t[b.c - 1][b.r].start();
 		}
 	}
-	if (b.c < t.length - 1) {
-		// right
-		if (d1.c !== b.c + 1 || d1.r !== b.r) {
+	if (b.c < 1) {
+		// consider right
+		if (!left_of_u1) {
 			t[b.c + 1][b.r].start();
+
 		}
 	}
-	if (b.r < t[0].length - 1) {
-		// down
-		if (d1.c !== b.c || d1.r !== b.r + 1) {
-			t[b.c][b.r + 1].start();
-		}
+	if (b.r < 3) {
+		// consider down
+		t[b.c][b.r + 1].start();
 	}
 	if (b.r > 0) {
-		t[b.c][b.r - 1].start();
+		//consider up
+		if (!under_u1) {
+			t[b.c][b.r - 1].start();
+		}
 	}
 }
 
@@ -96,6 +112,6 @@ window.addEventListener('load', () => {
 	grid.start();
 	border.start();
 	b_loop.start();
-	d1_loop.start();
+	u1_loop.start();
 	view();
 });
